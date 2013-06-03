a2af3a4064071f06fd1a619adda7f344774298d6
Re van Dorst <opensource@vdorst.com>
2013-06-03 11:15:29 +0200

    XBMC: disable ccache if buildroot ccache is not set.

--- package/thirdparty/xbmc/xbmc.mk
+++ package/thirdparty/xbmc/xbmc.mk
@@ -14,7 +14,10 @@ XBMC_DEPENDENCIES = host-lzo host-sdl_image
 
 XBMC_CONF_OPT+= --enable-neon --enable-gles --disable-sdl --disable-x11 --disable-xrandr \
   --disable-projectm --enable-debug --disable-joystick --with-cpu=cortex-a9 \
-  --enable-codec=amcodec --enable-player=amlplayer
+  --enable-codec=amcodec --enable-player=amlplayer 
+ifneq ($(BR2_CCACHE),y)
+XBMC_CONF_OPT+= --disable-ccache
+endif
 
 XBMC_DEPENDENCIES += libogg flac libmad libmpeg2 libogg \
   libsamplerate libtheora libvorbis wavpack bzip2 dbus libcdio \
@@ -71,7 +74,7 @@ endef
 endif
 
 define XBMC_INSTALL_SPLASH
-  cp -f package/thirdparty/xbmc/tlbb_splash.png $(TARGET_DIR)/usr/share/xbmc/media/Splash.png
+  cp -f package/thirdparty/xbmc/splash.png $(TARGET_DIR)/usr/share/xbmc/media/Splash.png
 endef
 
 define XBMC_CLEAN_UNUSED_ADDONS
