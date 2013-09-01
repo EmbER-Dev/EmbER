47f98fcdc9b6da68de30fe050f6847f2cfeb1d36
Gustavo Zacarias <gustavo@zacarias.com.ar>
2013-09-01 16:55:01 -0300

    Squashed commits for libmicrohttpd (version 0.9.31)
    libmicrohttpd: bump to version 0.9.29
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>
    
    libmicrohttpd: disable spdy support
    
    It requires OpenSSL and IPv6 support and it should probably be an
    option since SSL support uses GnuTLS thus making a huge target
    footprint with 2 SSL/TLS libraries for full support.
    But most of all it's considered experimental.
    Fixes:
    http://autobuild.buildroot.net/results/836/836c39c11cecbcf9399675ba3ab01202417b9642/
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>
    
    libmicrohttpd: bump to version 0.9.30
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <jacmet@sunsite.dk>
    
    libmicrohttpd: bump to version 0.9.31
    
    Signed-off-by: Gustavo Zacarias <gustavo@zacarias.com.ar>
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>
    
    libmicrohttpd: disable epoll support on avr32
    
    As it isn't available.
    
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

--- package/libmicrohttpd/libmicrohttpd.mk
+++ package/libmicrohttpd/libmicrohttpd.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-LIBMICROHTTPD_VERSION = 0.9.27
+LIBMICROHTTPD_VERSION = 0.9.31
 LIBMICROHTTPD_SITE = $(BR2_GNU_MIRROR)/libmicrohttpd
 LIBMICROHTTPD_LICENSE_FILES = COPYING
 LIBMICROHTTPD_INSTALL_STAGING = YES
+LIBMICROHTTPD_CONF_OPT = --disable-spdy
 
 ifeq ($(BR2_PACKAGE_LIBMICROHTTPD_SSL),y)
 LIBMICROHTTPD_LICENSE = LGPLv2.1+
@@ -17,6 +18,11 @@ LIBMICROHTTPD_CONF_OPT += --enable-https --with-gnutls=$(STAGING_DIR)/usr \
 else
 LIBMICROHTTPD_LICENSE = LGPLv2.1+ or eCos
 LIBMICROHTTPD_CONF_OPT += --disable-https
+endif
+
+ifeq ($(BR2_avr32),y)
+# no epoll_create1
+LIBMICROHTTPD_CONF_OPT += --disable-epoll
 endif
 
 $(eval $(autotools-package))
