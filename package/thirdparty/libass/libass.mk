da59eafd04ef53e8601841446faeabba644b3e33
croniccorey <cronmod.dev@gmail.com>
2013-11-20 22:32:22 -0500

    PACKAGE: Bump libass to 0.10.2

--- package/thirdparty/libass/libass.mk
+++ package/thirdparty/libass/libass.mk
@@ -3,11 +3,12 @@
 # libass
 #
 #############################################################
-LIBASS_VERSION = 0.9.12
+LIBASS_VERSION = 0.10.2
 LIBASS_SITE = http://libass.googlecode.com/files
 LIBASS_SOURCE = libass-$(LIBASS_VERSION).tar.gz
 LIBASS_INSTALL_STAGING = YES
 LIBASS_INSTALL_TARGET = YES
 LIBASS_DEPENDENCIES += libenca
+LIBASS_DEPENDENCIES += fontconfig
 
 $(eval $(call autotools-package,package/thirdparty,libass))
