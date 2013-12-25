4da4fcde222212176f53ce0676fe7e4aefc0ec3b
croniccorey <cronmod.dev@gmail.com>
2013-12-25 05:47:47 -0500

    RECOVERY_AML: Misc changes to install script

--- fs/recovery_aml/recovery_aml.mk
+++ fs/recovery_aml/recovery_aml.mk
@@ -140,8 +140,14 @@ ROOTFS_RECOVERY_AML_CMD += \
     mkdir -p $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
     PYTHONDONTWRITEBYTECODE=1 $(HOST_DIR)/usr/bin/python fs/recovery_aml/android_scriptgen $(RECOVERY_AML_ARGS) -i -p $(BINARIES_DIR)/aml_recovery/system -o \
      $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/updater-script && \
-    cp -f fs/recovery_aml/update-binary $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
-    cp -f $(AML_LOGO) $(BINARIES_DIR)/aml_recovery/aml_logo.img && \
+    cp -f fs/recovery_aml/update-binary $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ &&
+
+ifneq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK),y)
+ROOTFS_RECOVERY_AML_CMD += \
+    cp -f $(AML_LOGO) $(BINARIES_DIR)/aml_recovery/aml_logo.img &&
+endif
+
+ROOTFS_RECOVERY_AML_CMD += \
     cp -f $(BINARIES_DIR)/uImage $(BINARIES_DIR)/aml_recovery/ && \
     find $(BINARIES_DIR)/aml_recovery/system/ -type l -delete && \
     find $(BINARIES_DIR)/aml_recovery/system/ -type d -empty -exec sh -c 'echo "dummy" > "{}"/.empty' \; && \
