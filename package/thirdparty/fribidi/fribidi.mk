0e47a77789691d5de8b08b30f00c7b88c8be23cb
croniccorey <cronmod.dev@gmail.com>
2014-01-05 00:23:09 -0500

    THIRDPARTY: Bump fribidi from 0.19.1 > 0.19.6

--- package/thirdparty/fribidi/fribidi.mk
+++ package/thirdparty/fribidi/fribidi.mk
@@ -3,8 +3,8 @@
 # fribidi
 #
 #############################################################
-FRIBIDI_VERSION = 0.19.1
-FRIBIDI_SOURCE = fribidi-$(FRIBIDI_VERSION).tar.gz
+FRIBIDI_VERSION = 0.19.6
+FRIBIDI_SOURCE = fribidi-$(FRIBIDI_VERSION).tar.bz2
 FRIBIDI_SITE = http://fribidi.org/download
 FRIBIDI_INSTALL_STAGING = YES
 FRIBIDI_INSTALL_TARGET = YES
