d058a321f399a45fea3f22236e332729c9cc9076
croniccorey <cronmod.dev@gmail.com>
2014-01-05 00:39:47 -0500

    THIRDPARTY: Bump libmodplug from 0.8.7 > 0.8.8.4

--- package/thirdparty/libmodplug/libmodplug.mk
+++ package/thirdparty/libmodplug/libmodplug.mk
@@ -3,8 +3,8 @@
 # libmodplug
 #
 #############################################################
-LIBMODPLUG_VERSION = 0.8.7
-LIBMODPLUG_SITE = http://downloads.sourceforge.net/project/modplug-xmms/libmodplug/0.8.7
+LIBMODPLUG_VERSION = 0.8.8.4
+LIBMODPLUG_SITE = http://downloads.sourceforge.net/project/modplug-xmms/libmodplug/$(LIBMODPLUG_VERSION)
 LIBMODPLUG_SOURCE = libmodplug-$(LIBMODPLUG_VERSION).tar.gz
 LIBMODPLUG_INSTALL_STAGING = YES
 LIBMODPLUG_INSTALL_TARGET = YES
