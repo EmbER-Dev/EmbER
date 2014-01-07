c0c3c0c69f258f818b0491f6d5e881db449580a6
croniccorey <cronmod.dev@gmail.com>
2014-01-07 04:18:51 -0500

    AML: Add Broadcom WIFI support package

---
+++ package/amlogic/broadcom/broadcom.mk
@@ -0,0 +1,32 @@
+#############################################################
+#
+# broadcom wifi support
+#
+#############################################################
+BROADCOM_VERSION = aml
+BROADCOM_SOURCE = broadcom-$(BROADCOM_VERSION)-firmware.tar.gz
+BROADCOM_SITE = $(TOPDIR)/package/amlogic/broadcom/src
+BROADCOM_SITE_METHOD = local
+BROADCOM_INSTALL_STAGING = NO
+BROADCOM_INSTALL_TARGET = YES
+
+define BROADCOM_INSTALL_TARGET_CMDS
+ mkdir -p $(TARGET_DIR)/etc/init.d
+ mkdir -p $(TARGET_DIR)/etc/wifi/4330
+ mkdir $(TARGET_DIR)/etc/wifi/40181
+ mkdir $(TARGET_DIR)/etc/wifi/40183
+ mkdir $(TARGET_DIR)/etc/wifi/ap62x2
+ mkdir $(TARGET_DIR)/etc/wifi/ap6181
+ mkdir $(TARGET_DIR)/etc/wifi/ap6210
+ mkdir $(TARGET_DIR)/etc/wifi/ap6330
+ install -m 755 $(@D)/S60wifi $(TARGET_DIR)/etc/init.d
+ install -m 644 $(@D)/firmware/4330/*.bin $(TARGET_DIR)/etc/wifi/4330
+ install -m 644 $(@D)/firmware/40181/*.bin $(TARGET_DIR)/etc/wifi/40181
+ install -m 644 $(@D)/firmware/40183/*.bin $(TARGET_DIR)/etc/wifi/40183
+ install -m 644 $(@D)/firmware/ap62x2/*.bin $(TARGET_DIR)/etc/wifi/ap62x2
+ install -m 644 $(@D)/firmware/ap6181/*.bin $(TARGET_DIR)/etc/wifi/ap6181
+ install -m 644 $(@D)/firmware/ap6210/*.bin $(TARGET_DIR)/etc/wifi/ap6210
+ install -m 644 $(@D)/firmware/ap6330/*.bin $(TARGET_DIR)/etc/wifi/ap6330
+endef
+
+$(eval $(call generic-package))
