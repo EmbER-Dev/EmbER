86a2d68cc17d5ee6fbc7f7bcc0ae32a3fc3ffef4
croniccorey <cronmod.dev@gmail.com>
2014-01-20 11:20:58 -0500

    Use NTFS-3g for NTFS disk The kernel NTFS driver does not support big disks ( > 2.2 TB ) - Enable ntfs-3g package - Use util-linux mount binaries for mounting instead of busybox - Use busybox mount for CIFS and NFS
    (Based on Toys4me PR from old Pivos repo)

--- package/util-linux/util-linux.mk
+++ package/util-linux/util-linux.mk
@@ -27,7 +27,15 @@ HOST_UTIL_LINUX_DEPENDENCIES = host-pkgconf
 # wins the fight over who gets to have their utils actually installed
 ifeq ($(BR2_PACKAGE_BUSYBOX),y)
 UTIL_LINUX_DEPENDENCIES += busybox
+
+# If we want to use busybox cifs/nfs mounts
+# we need to create some helpers to have busybox handle those mounts
+define UTIL_LINUX_TARGET_HELPERS
+        $(INSTALL) -m 0755 package/util-linux/mount.nfs $(TARGET_DIR)/sbin
+	$(INSTALL) -m 0755 package/util-linux/mount.cifs $(TARGET_DIR)/sbin
+endef
 endif
+UTIL_LINUX_POST_INSTALL_TARGET_HOOKS += UTIL_LINUX_TARGET_HELPERS
 
 ifeq ($(BR2_PACKAGE_NCURSES),y)
 UTIL_LINUX_DEPENDENCIES += ncurses
