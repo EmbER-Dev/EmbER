b1960fe3e8b7d72fc278de5ffd4eb119ded4b788
Stanislav Vlasic <svlasic@gmail.com>
2014-04-29 16:14:30 +0200

    [recovery_aml] Fix append ramdisk to kernel

--- fs/recovery_aml/recovery_aml.mk
+++ fs/recovery_aml/recovery_aml.mk
@@ -191,9 +191,9 @@ ifneq ($(qstrip $(BR2_TARGET_ROOTFS_RECOVERY_AML_APPEND_INITRD)),)
 
 ROOTFS_RECOVERY_AML_CMD += \
     echo "Appending initramfs to kernel..." && \
-    cd $(RECOVERY_AML_BUILDROOT_ROOT)/$(BR2_TARGET_ROOTFS_RECOVERY_AML_APPEND_INITRD)/ && \
+    cd $(BASE_DIR)/../$(BR2_TARGET_ROOTFS_RECOVERY_AML_APPEND_INITRD)/ && \
     find . | cpio -o --format=newc | gzip > $(BINARIES_DIR)/aml_recovery/ramdisk-new.gz && \
-    cd $(RECOVERY_AML_BUILDROOT_ROOT) && \
+    cd $(BASE_DIR)/../ && \
     fs/recovery_aml/mkbootimg --kernel $(BINARIES_DIR)/uImage --ramdisk $(BINARIES_DIR)/aml_recovery/ramdisk-new.gz -o $(BINARIES_DIR)/aml_recovery/uImage && \
     cp -f $(BINARIES_DIR)/aml_recovery/uImage $(BINARIES_DIR)/kernel &&  
 else
