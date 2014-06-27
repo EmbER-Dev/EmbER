58a41e64594c622da0eebf8c827ac84e6fe0dad8
j1nx <p.steenbergen@j1nx.nl>
2014-06-27 21:40:50 +0200

    Bump ntfs-3g package

--- package/ntfs-3g/ntfs-3g.mk
+++ package/ntfs-3g/ntfs-3g.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NTFS_3G_VERSION = 2013.1.13
+NTFS_3G_VERSION = 2014.2.15
 NTFS_3G_SOURCE = ntfs-3g_ntfsprogs-$(NTFS_3G_VERSION).tgz
 NTFS_3G_SITE = http://tuxera.com/opensource
 NTFS_3G_CONF_OPT = --disable-ldconfig
@@ -31,11 +31,5 @@ endif
 ifneq ($(BR2_PACKAGE_NTFS_3G_NTFSPROGS),y)
 	NTFS_3G_CONF_OPT += --disable-ntfsprogs
 endif
-
-define NTFS_3G_TARGET_SYMLINK_CREATE
-	cd $(TARGET_DIR)/sbin; ln -fs mount.ntfs-3g mount.ntfs
-endef
-
-NTFS_3G_POST_INSTALL_TARGET_HOOKS += NTFS_3G_TARGET_SYMLINK_CREATE
 
 $(eval $(autotools-package))
